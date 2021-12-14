650249432a1142818a580ebfa580b7ccd90c73a2
RoyAppeldoorn <roy.appeldoorn@gmail.com>
2021-12-14 11:54:08 +0100

    Initial commit

--- ARworkshop/ContentView.swift
+++ ARworkshop/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
@@ -16,21 +17,85 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    let arView = ARView(frame: .zero)
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(arView: arView)
+    }
+    
     func makeUIView(context: Context) -> ARView {
+
+        let config = ARImageTrackingConfiguration();
+        config.maximumNumberOfTrackedImages = 1
+        config.isAutoFocusEnabled = true
         
-        let arView = ARView(frame: .zero)
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Images not found")
+        }
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        config.trackingImages = trackingImages
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.session.delegate = context.coordinator
+        arView.session.run(config, options: [])
         
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+    class Coordinator: NSObject, ARSessionDelegate {
+        
+        let arView: ARView!
+        var avPlayer: AVPlayer?
+        
+        init(arView: ARView) {
+            self.arView = arView
+        }
+        
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            for anchor in anchors {
+                if let imageAnchor = anchor as? ARImageAnchor {
+                    if imageAnchor.isTracked {
+                        avPlayer?.play()
+                    } else {
+                        avPlayer?.pause()
+                    }
+                }
+            }
+        }
+        
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            
+            for anchor in anchors {
+                if let imageAnchor = anchor as? ARImageAnchor {
+                    
+                    let anchorEntity = AnchorEntity(anchor: imageAnchor)
+                    
+                    let width = Float(imageAnchor.referenceImage.physicalSize.width)
+                    let height = Float(imageAnchor.referenceImage.physicalSize.height)
+                    
+                    guard let videoUrl = Bundle.main.url(forResource: imageAnchor.name, withExtension: "mp4") else {
+                        fatalError("Video not found")
+                    }
+                    
+                    let playerItem = AVPlayerItem(url: videoUrl)
+                    avPlayer = AVPlayer(playerItem: playerItem)
+                    let videoMaterial = VideoMaterial(avPlayer: avPlayer!)
+                    
+                    
+                    let modelEntity = ModelEntity(mesh: .generatePlane(width: width, depth: height), materials: [videoMaterial])
+                    
+                    anchorEntity.addChild(modelEntity)
+                    
+                    arView.scene.addAnchor(anchorEntity)
+                }
+                
+            }
+            
+        }
+
+    }
     
 }
 
