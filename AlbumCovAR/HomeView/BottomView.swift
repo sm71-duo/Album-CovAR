215296d563eca41289fd22d4907ac0d8085e26e3
Coen Breg <coenbreg@gmail.com>
2021-12-22 14:11:23 +0100

    added albums to homeview

--- AlbumCovAR/HomeView/BottomView.swift
+++ AlbumCovAR/HomeView/BottomView.swift
@@ -7,38 +7,53 @@
 
 import SwiftUI
 
-let GradientBorder = LinearGradient(gradient: Gradient(colors: [Color("CustomBlue"), Color("CustomLightBlue").opacity(0.0)]), startPoint: .top, endPoint: .bottom)
+let gradientBorder = LinearGradient(gradient: Gradient(colors: [Color("CustomBlue"), Color("CustomLightBlue").opacity(0.0)]), startPoint: .top, endPoint: .bottom)
+
+
 
 struct BottomView: View {
+    
+    let recentAlbums: [Album] = [
+        Album(name: "Flower Boy", artist: "Tyler, the Creator", coverImageName: "FlowerBoyAlbum"),
+        Album(name: "Sand", artist: "Balthazar", coverImageName: "SandAlbum"),
+        Album(name: "Skin", artist: "Flume", coverImageName: "SkinAlbum")
+    ]
+    
     var body: some View {
         VStack{
-            HStack() {
+            HStack(alignment: .top) {
                 Text("Recent Albums")
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .font(.title3)
                     .padding(.horizontal)
-                Spacer()
             }
             
+            ScrollView{
+                ForEach(recentAlbums) {album in
+                    AlbumComponentView(album: album)
+                }
+                .listStyle(.plain)
+            }
+            .frame(height: 200)
             
         }
         .padding(.vertical)
         .frame(minWidth: 0, maxWidth: .infinity)
         .overlay(
-               RoundedRectangle(cornerRadius: 24)
-                   .stroke(GradientBorder, lineWidth: 4))
+            RoundedRectangle(cornerRadius: 24)
+                .stroke(gradientBorder, lineWidth: 4))
         .padding()
-
-        
     }
+    
+    
 }
 
 struct BottomView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
             Color("BackgroundColor").ignoresSafeArea()
-        BottomView()
+            BottomView()
         }
     }
 }
--- AlbumCovAR/HomeView/HomeView.swift
+++ AlbumCovAR/HomeView/HomeView.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 
-
-
 struct HomeView: View {
     var body: some View {
         NavigationView {
