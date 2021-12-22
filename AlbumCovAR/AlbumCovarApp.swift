1dc8facfbb5a915f2b4eae090da2949691f34fe8
Coen Breg <coenbreg@gmail.com>
2021-12-22 15:00:22 +0100

    made new main

--- AlbumCovAR/AlbumCovarApp.swift
+++ AlbumCovAR/AlbumCovarApp.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct AlbumCovarApp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@main
+struct AlbumCovarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                HomeView()
+            }
+        }
     }
 }
 
-struct AlbumCovarApp_Previews: PreviewProvider {
-    static var previews: some View {
-        AlbumCovarApp()
-    }
-}
