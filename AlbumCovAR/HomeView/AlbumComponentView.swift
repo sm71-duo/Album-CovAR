716c7a229ed5730b01d443ed8206905c79b72f20
Coen Breg <coenbreg@gmail.com>
2021-12-22 13:27:54 +0100

    init album component

--- AlbumCovAR/HomeView/AlbumComponentView.swift
+++ AlbumCovAR/HomeView/AlbumComponentView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct AlbumComponentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
 struct AlbumComponentView_Previews: PreviewProvider {
     static var previews: some View {
-        AlbumComponentView()
+        ZStack {
+            Color("BackgroundColor").ignoresSafeArea()
+            AlbumComponentView()
+        }
     }
 }
