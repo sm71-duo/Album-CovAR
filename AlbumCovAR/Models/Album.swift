cb1ca77d1ff2ba6c84b0f94b61cb3707e7ca212a
Coen Breg <coenbreg@gmail.com>
2021-12-23 11:13:44 +0100

    added new models

--- AlbumCovAR/Models/Album.swift
+++ AlbumCovAR/Models/Album.swift
@@ -12,10 +12,83 @@ struct Album: Identifiable {
     var name: String
     var artist: String
     var coverImageName: String
+    var tracklist: Tracklist
     
-    init(name: String, artist: String, coverImageName: String) {
+    init(name: String, artist: String, coverImageName: String, tracklist: Tracklist) {
         self.name = name
         self.artist = artist
         self.coverImageName = coverImageName
+        self.tracklist = tracklist
     }
 }
+
+extension Album {
+    static let sampleData: [Album] =
+    [
+        Album(
+            name: "Flower Boy",
+            artist: "Tyler, the Creator",
+            coverImageName: "FlowerBoyAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "Sand",
+            artist: "Balthazar",
+            coverImageName: "SandAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "Skin",
+            artist: "Flume",
+            coverImageName: "SkinAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "Damn",
+            artist: "Kendrick Lamar",
+            coverImageName: "DamnAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "For Ever",
+            artist: "Jungle",
+            coverImageName: "ForEverAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "Loving In Stereo",
+            artist: "Jungle",
+            coverImageName: "LovingInStereoAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "KIDS SEE GHOSTS",
+            artist: "KIDS SEE GHOSTS",
+            coverImageName: "KidsSeeGhostsAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        ),
+        Album(
+            name: "Hi, this is Flume",
+            artist: "Flume",
+            coverImageName: "HiThisIsFlumeAlbum",
+            tracklist: Tracklist(
+                songs: []
+            )
+        )
+    ]
+}
+
---
+++ AlbumCovAR/Models/Song.swift
@@ -0,0 +1,21 @@
+//
+//  Song.swift
+//  AlbumCovAR
+//
+//  Created by Quan on 23/12/2021.
+//
+
+import Foundation
+
+struct Song: Identifiable {
+    var id = UUID()
+    var name: String
+    var artist: String
+    var trackNumber: Int
+    
+    init(name: String, artist: String, trackNumber: Int) {
+        self.name = name
+        self.artist = artist
+        self.trackNumber = trackNumber
+    }
+}
