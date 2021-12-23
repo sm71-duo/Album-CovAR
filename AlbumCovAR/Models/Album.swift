4e5b3b91e2759748161a2d203cdb2ec2bcf458fb
Coen Breg <coenbreg@gmail.com>
2021-12-23 11:22:11 +0100

    added tracklist flowerboy

--- AlbumCovAR/Models/Album.swift
+++ AlbumCovAR/Models/Album.swift
@@ -30,7 +30,79 @@ extension Album {
             artist: "Tyler, the Creator",
             coverImageName: "FlowerBoyAlbum",
             tracklist: Tracklist(
-                songs: []
+                songs: [
+                    Song(
+                        name: "Foreword",
+                        artist: ["Tyler, The Creator", "Rex Orange County"],
+                        trackNumber: 1
+                    ),
+                    Song(
+                        name: "Where This Flower Blooms",
+                        artist: ["Tyler, The Creator", "Frank Ocean"],
+                        trackNumber: 2
+                    ),
+                    Song(
+                        name: "Sometimes...",
+                        artist: ["Tyler, The Creator"],
+                        trackNumber: 3
+                    ),
+                    Song(
+                        name: "See You Again",
+                        artist: ["Tyler, The Creator", "See You Again"],
+                        trackNumber: 4
+                    ),
+                    Song(
+                        name: "Who Dat Boy",
+                        artist: ["Tyler, The Creator", "A$AP Rocky"],
+                        trackNumber: 5
+                    ),
+                    Song(
+                        name: "Pothole",
+                        artist: ["Tyler, The Creator", "Jaden"],
+                        trackNumber: 6
+                    ),
+                    Song(
+                        name: "Garden Shed",
+                        artist: ["Tyler, The Creator", "Estelle"],
+                        trackNumber: 7
+                    ),
+                    Song(
+                        name: "Boredom",
+                        artist: ["Tyler, The Creator", "Rex Orange County", "Anna of the North"],
+                        trackNumber: 8
+                    ),
+                    Song(
+                        name: "I Ain't Got Time!",
+                        artist: ["Tyler, The Creator"],
+                        trackNumber: 9
+                    ),
+                    Song(
+                        name: "911/Mr. Lonely",
+                        artist: ["Tyler, The Creator", "Frank Ocean", "Steve Lacy"],
+                        trackNumber: 10
+                    ),
+                    Song(
+                        name: "Droppin' Seeds",
+                        artist: ["Tyler, The Creator", "Lil' Wayne"],
+                        trackNumber: 11
+                    ),
+                    Song(
+                        name: "November",
+                        artist: ["Tyler, The Creator"],
+                        trackNumber: 12
+                    ),
+                    Song(
+                        name: "Glitter",
+                        artist: ["Tyler, The Creator"],
+                        trackNumber: 13
+                    ),
+                    Song(
+                        name: "Enjoy Right Now, Today",
+                        artist: ["Tyler, The Creator", "Jaden"],
+                        trackNumber: 14
+                    ),
+                    
+                ]
             )
         ),
         Album(
--- AlbumCovAR/Models/Song.swift
+++ AlbumCovAR/Models/Song.swift
@@ -10,10 +10,10 @@ import Foundation
 struct Song: Identifiable {
     var id = UUID()
     var name: String
-    var artist: String
+    var artist: [String]
     var trackNumber: Int
     
-    init(name: String, artist: String, trackNumber: Int) {
+    init(name: String, artist: [String], trackNumber: Int) {
         self.name = name
         self.artist = artist
         self.trackNumber = trackNumber
