b966f762ab4d042b7998a7357f2c97cbee599994
Coen Breg <coenbreg@gmail.com>
2021-12-24 13:42:07 +0100

    fix: cofing issue, app break on state change

--- AlbumCovAR/AlbumARView.swift
+++ AlbumCovAR/AlbumARView.swift
@@ -11,14 +11,14 @@ import ARKit
 
 struct AlbumARView : View {
     @State private var showBottomSheet = false
-    @State private var albumTitle = ""
+    @State  var albumTitle = ""
     
     let testAlbum = Album.sampleData[2]
     
     var body: some View {
         ZStack(alignment: .bottomLeading) {
             ARViewContainer(albumTitle: $albumTitle, showBottomSheet: $showBottomSheet).edgesIgnoringSafeArea(.all)
-
+            
             Button(action: {
                 withAnimation {
                     self.showBottomSheet.toggle()
@@ -37,90 +37,103 @@ struct AlbumARView : View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
-
-    let arView = ARView(frame: .zero)
-    let albumTitle: Binding<String>
+    
+    let arView = ARView(frame: .init(x: 1, y: 1, width: 1, height: 1), cameraMode: .ar, automaticallyConfigureSession: false)
+    @Binding var albumTitle: String
     let showBottomSheet: Binding<Bool>
-
+    
     func makeCoordinator() -> Coordinator {
-        Coordinator(arView: arView, albumTitleState: albumTitle, showBottomSheetState: showBottomSheet)
+        Coordinator(arView: arView, albumTitle: $albumTitle, showBottomSheetState: showBottomSheet)
     }
-
+    
     func makeUIView(context: Context) -> ARView {
-
+        
         let config = ARImageTrackingConfiguration();
         config.maximumNumberOfTrackedImages = 2
         config.isAutoFocusEnabled = true
-
+        
         guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
             fatalError("Images not found")
         }
-
+        
         config.trackingImages = trackingImages
-
+        
         arView.session.delegate = context.coordinator
         arView.session.run(config, options: [])
-
+        
         arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(recognizer:))))
-
+        
         return arView
-
+        
     }
-
+    
     func updateUIView(_ uiView: ARView, context: Context) {}
-
+    
     class Coordinator: NSObject, ARSessionDelegate {
-
+        
         let arView: ARView!
-        let albumTitle: Binding<String>
+        @Binding var albumTitle: String
         let showBottomSheet: Binding<Bool>
-
-        init(arView: ARView, albumTitleState: Binding<String>, showBottomSheetState: Binding<Bool>) {
+        
+        init(arView: ARView, albumTitle: Binding<String>, showBottomSheetState: Binding<Bool>) {
             self.arView = arView
-            self.albumTitle = albumTitleState
+            self._albumTitle = albumTitle
             self.showBottomSheet = showBottomSheetState
         }
-
+        
         @objc func handleTap(recognizer: UITapGestureRecognizer) {
             let location = recognizer.location(in: arView)
-
+            
             if let tappedEntity = arView.entity(at: location) {
                 showBottomSheet.wrappedValue = true
-                albumTitle.wrappedValue = tappedEntity.name
-                print("Found album: \(tappedEntity.name)")
+                albumTitle = tappedEntity.name
+                print("UPDATE: Found album: \(tappedEntity.name)")
             }
         }
-
+        
         func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
             for anchor in anchors {
                 if let imageAnchor = anchor as? ARImageAnchor {
-
+                    
                     let anchorEntity = AnchorEntity(anchor: imageAnchor)
-
+                    
                     let width = Float(imageAnchor.referenceImage.physicalSize.width)
                     let height = Float(imageAnchor.referenceImage.physicalSize.height)
-
+                    
                     var albumOverlay = SimpleMaterial()
                     albumOverlay.color = try! .init( texture: .init(.load(named: "\(anchor.name ?? "help") Cover", in: nil)))
                     let albumEntity = ModelEntity(mesh: .generatePlane(width: width * 1.1, depth: height * 1.1, cornerRadius: 0.01), materials: [albumOverlay])
                     albumEntity.name = anchor.name ?? "'no name found'"
-//                    albumEntity.generateCollisionShapes(recursive: true)
-
+                    //                    albumEntity.generateCollisionShapes(recursive: true)
+                    
                     let pressable = SimpleMaterial(color: .lightGray.withAlphaComponent(0.0), isMetallic: true)
                     let pressableEntity = ModelEntity(mesh: .generatePlane(width: width / 4, depth: height / 4, cornerRadius: 9999), materials: [pressable])
                     pressableEntity.generateCollisionShapes(recursive: true)
                     pressableEntity.name = anchor.name ?? "'no name found'"
                     pressableEntity.setPosition(SIMD3<Float>(0, 0.05, 0), relativeTo: anchorEntity)
-
+                    
                     anchorEntity.addChild(pressableEntity)
                     anchorEntity.addChild(albumEntity)
                     arView.scene.addAnchor(anchorEntity)
-
+                    
+                }
+            }
+        }
+        
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            for anchor in anchors {
+                if let imageAnchor = anchor as? ARImageAnchor {
+                    if imageAnchor.isTracked{
+                        print("UPDATE: tracking")
+                    } else {
+                        print("UPDATE: removed")
+                    }
                 }
             }
         }
     }
 }
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
